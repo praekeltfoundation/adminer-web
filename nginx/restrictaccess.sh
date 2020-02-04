498b36ae0ad21b27a2875420646bee67a3a6d7ea
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-02-04 15:51:06 +0200

    add server name

--- nginx/restrictaccess.sh
+++ nginx/restrictaccess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+# Add auth_basic if HTTP_AUTH_LOGIN & HTTP_AUTH_PASSWORD
 if [ "${HTTP_AUTH_LOGIN}" != "" -a "${HTTP_AUTH_PASSWORD}" != ""  ]; then
   sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/adminer.conf
   rm -rf /etc/nginx/.htpasswd
@@ -10,9 +12,15 @@ else
 fi
 
 
+# Add allowed ip address if HTTP_AUTH_IP
 if [ "${HTTP_AUTH_IP}" != "" ];then
   sed -i "s/#allow ip/allow ${HTTP_AUTH_IP}/g;" /etc/nginx/conf.d/adminer.conf
   sed -i "s/#deny all/deny all/g;" /etc/nginx/conf.d/adminer.conf
   echo "deny all; allow ${HTTP_AUTH_IP}"
 fi
 
+# update server name
+if [ "${ALLOWED_HOST}" != "" ];then
+  sed -i "s/server_name adminer;/server_name ${ALLOWED_HOST}/g;" /etc/nginx/conf.d/adminer.conf
+  echo "set server_name  ${ALLOWED_HOST}"
+fi
